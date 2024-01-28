7d0c07455d74c5bfe266b2d6f81ff488da903f83
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-01-29 00:33:49 +0600

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Outlet, Route, Routes } from 'react-router-dom';
+import EmptyScreen from './components/EmptyScreen';
 import RootLayout from './layouts/RootLayout';
 import Home from './pages/home/Home';
+import Notification from './pages/notification/Notification';
 
 function App() {
 	return (
@@ -8,6 +10,11 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Outlet />}>
 					<Route index element={<Home />} />
+					<Route path='explore' element={<EmptyScreen />} />
+					<Route path='notifications' element={<Notification />} />
+					<Route path='communities' element={<EmptyScreen />} />
+					<Route path='bookmarks' element={<EmptyScreen />} />
+					<Route path='profile' element={<EmptyScreen />} />
 				</Route>
 			</Routes>
 		</RootLayout>
