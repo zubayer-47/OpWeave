b3633cb0bdd454c06ddcdbe64e08fc752e105138
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-02-18 10:59:03 +0600

    add access permissions into nav-links and more

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,37 +1,111 @@
-import { Bell, ChevronDown, Compass, Home, Mail } from 'lucide-react';
-import { NavLink, useLocation } from 'react-router-dom';
+import {
+	Bell,
+	ChevronDown,
+	Compass,
+	Home,
+	LucideIcon,
+	Mail,
+} from 'lucide-react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import PermissionWrapper from '../Routes/PermissionWrapper';
 import logo from '../assets/opweave.webp';
 import profile from '../assets/profile.webp';
+import { UserRight } from '../contexts/user/types';
+import useModal from '../hooks/useModal';
 import SubModal from './Modals/SubModal';
 
-const navLinks = [
-	{ path: '/', label: 'Home', Icon: Home },
-	{ path: '/explore', label: 'Explore', Icon: Compass },
-	{ path: '/notifications', label: 'Notifications', Icon: Bell },
-	{ path: '/chat', label: 'Chat', Icon: Mail },
+type NavLinkType = {
+	path: string;
+	label: string;
+	Icon: LucideIcon;
+	permission: UserRight[] | UserRight;
+};
+
+const permissions = {
+	all: [UserRight.ADMIN, UserRight.MODER, UserRight.USER],
+	user: UserRight.USER,
+	mod: UserRight.MODER,
+};
+
+const navLinks: NavLinkType[] = [
+	{ path: '/', label: 'Home', Icon: Home, permission: permissions.all },
+	{
+		path: '/explore',
+		label: 'Explore',
+		Icon: Compass,
+		permission: permissions.all,
+	},
+	{
+		path: '/notifications',
+		label: 'Notifications',
+		Icon: Bell,
+		permission: permissions.all,
+	},
+	{ path: '/chat', label: 'Chat', Icon: Mail, permission: permissions.all },
+	{ path: '/auth', label: 'Auth', Icon: Mail, permission: permissions.all },
 ];
 
 const Nav = () => {
+	// const { state } = useAuth();
 	const location = useLocation();
 	const pathname = location.pathname;
 	// console.log('pathname :', pathname);
+	const { updateModal } = useModal();
 
 	const activeItem = navLinks.find((link) => link.path === pathname);
 	// console.log('activeItem :', activeItem);
 
+	const handleLoginModal = () => updateModal(true);
+
 	return (
 		<nav className='absolute w-full h-[4.75rem] grid items-center bg-primary z-10 shadow-3xl px-10'>
 			<div className='flex justify-between items-center'>
-				<div className='flex items-center'>
-					<img src={logo} className='profile' alt='OpWeave logo' />
-					<input type='search' name='search' />
+				<div className='flex items-center gap-5'>
+					<Link to='/'>
+						<img src={logo} className='profile size-14' alt='OpWeave logo' />
+					</Link>
+
+					<form>
+						<label
+							htmlFor='search'
+							className='mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white'
+						>
+							Search
+						</label>
+						<div className='relative'>
+							<div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
+								<svg
+									className='size-4 dark:text-dark-muted'
+									aria-hidden='true'
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 20 20'
+								>
+									<path
+										stroke='currentColor'
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth='2'
+										d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
+									/>
+								</svg>
+							</div>
+							<input
+								type='search'
+								name='search'
+								className='block w-full px-3 py-2.5 ps-10 text-sm text-dark rounded-lg focus:outline-none border focus:border-nav-selected dark:bg-secondary dark:border-dark-border dark:placeholder-dark-muted dark:text-light dark:focus:border-blue-500'
+								placeholder='Search'
+								required
+							/>
+						</div>
+					</form>
 				</div>
 
 				<div className='flex items-center gap-5'>
 					<div className='flex items-center gap-4'>
 						{navLinks.map(({ Icon, label, path }) => (
+							// <PermissionWrapper key={path} permission={permission} links>
 							<NavLink
-								key={path}
 								to={path}
 								className={({ isActive }) =>
 									`nav-link transition-all duration-200 ${
@@ -40,30 +114,46 @@ const Nav = () => {
 								}
 							>
 								<Icon
+									// size={28}
 									className={`${
 										activeItem?.path === path
-											? 'text-nav-selected '
-											: 'text-dark-muted'
+											? 'size-6 text-nav-selected'
+											: 'size-7 text-dark-muted'
 									}`}
 								/>
 								<span className='animate-navlink-open'>
 									{activeItem?.path === path ? label : null}
 								</span>
 							</NavLink>
+							// </PermissionWrapper>
 						))}
+
+						<PermissionWrapper permission={UserRight.FREE} links>
+							<button
+								type='button'
+								className='button px-7 py-2.5 flex items-center gap-2'
+								onClick={handleLoginModal}
+							>
+								Log In
+							</button>
+						</PermissionWrapper>
 					</div>
 
-					<span className='w-1 bg-dark-muted/25 h-10 rounded-full'></span>
-
-					<button
-						type='button'
-						className='flex items-center gap-3 hover:bg-secondary px-3 py-1.5 rounded-2xl transition-colors relative'
+					<PermissionWrapper
+						permission={[UserRight.ADMIN, UserRight.MODER, UserRight.USER]}
+						links
 					>
-						<img className='profile' src={profile} alt='user profile' />
-						<h1 className='title'>A B M Zubayer</h1>
-						<ChevronDown className='text-dark-muted' />
+						<span className='w-1 bg-dark-muted/25 h-10 rounded-full'></span>
+						<button
+							type='button'
+							className='flex items-center gap-3 hover:bg-secondary px-3 py-1.5 rounded-2xl transition-colors relative'
+						>
+							<img className='profile' src={profile} alt='user profile' />
+							<h1 className='title'>A B M Zubayer</h1>
+							<ChevronDown className='text-dark-muted' />
+						</button>
 						<SubModal />
-					</button>
+					</PermissionWrapper>
 				</div>
 			</div>
 		</nav>
