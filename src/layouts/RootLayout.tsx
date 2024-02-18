3941a16628dad9b1c17395f794e5a9a9678a5260
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-02-18 11:00:28 +0600

    update: update RootLayout to make usable for route wrapper

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,19 +1,19 @@
-import { PropsWithChildren } from 'react';
+import { Outlet } from 'react-router-dom';
 import CommunitySuggestions from '../components/CommunitySuggestions';
 import CreatePost from '../components/CreatePost';
 import Nav from '../components/Nav';
 import useController from '../hooks/useController';
 
-const RootLayout = ({ children }: PropsWithChildren) => {
+const RootLayout = () => {
 	const {
 		state: { isPostModalOpen },
 	} = useController();
 	return (
 		<>
 			<Nav />
-			<div className='container mx-auto grid grid-cols-12 gap-2 px-20 pt-28 h-screen overflow-hidden'>
+			<div className='container mx-auto grid grid-cols-12 px-20 pt-20 h-screen overflow-hidden'>
 				<div className='col-span-8 h-full px-28 overflow-y-auto scrollbar-none'>
-					{children}
+					<Outlet />
 				</div>
 				<CommunitySuggestions />
 			</div>
