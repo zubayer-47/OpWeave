13438f1e058b75dc10e26fede3f6cb6a5ee7fc7a
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-02-18 10:56:28 +0600

    modified: make specific routes into their desired layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import PermissionWrapper from './Routes/PermissionWrapper';
 import EmptyScreen from './components/EmptyScreen';
+import { UserRight } from './contexts/user/types';
+import AuthLayout from './layouts/AuthLayout';
 import RootLayout from './layouts/RootLayout';
+import Auth from './pages/auth/Auth';
 import Bookmarks from './pages/bookmarks/Bookmarks';
 import Communities from './pages/communities/Communities';
 import Home from './pages/home/Home';
@@ -8,18 +12,48 @@ import Notification from './pages/notification/Notification';
 
 function App() {
 	return (
-		<RootLayout>
-			<Routes>
-				<Route path='/' element={<Outlet />}>
-					<Route index element={<Home />} />
-					<Route path='explore' element={<EmptyScreen />} />
-					<Route path='notifications' element={<Notification />} />
-					<Route path='communities' element={<Communities />} />
-					<Route path='bookmarks' element={<Bookmarks />} />
-					<Route path='profile' element={<EmptyScreen />} />
-				</Route>
-			</Routes>
-		</RootLayout>
+		// <RootLayout>
+		<Routes>
+			<Route path='/' element={<RootLayout />}>
+				<Route index element={<Home />} />
+
+				<Route path='explore' element={<EmptyScreen />} />
+				<Route
+					path='notifications'
+					element={
+						<PermissionWrapper
+							permission={[UserRight.ADMIN, UserRight.MODER, UserRight.USER]}
+							children={<Notification />}
+						/>
+					}
+				/>
+				<Route
+					path='communities'
+					element={
+						<PermissionWrapper
+							permission={[UserRight.ADMIN, UserRight.MODER, UserRight.USER]}
+							children={<Communities />}
+						/>
+					}
+				/>
+				<Route
+					path='bookmarks'
+					element={
+						<PermissionWrapper
+							permission={[UserRight.ADMIN, UserRight.MODER, UserRight.USER]}
+							children={<Bookmarks />}
+						/>
+					}
+				/>
+				<Route path='chat' element={<EmptyScreen />} />
+				<Route path='profile' element={<EmptyScreen />} />
+			</Route>
+
+			<Route path='auth' element={<AuthLayout />}>
+				<Route index element={<Auth />} />
+			</Route>
+		</Routes>
+		// </RootLayout>
 	);
 }
 
