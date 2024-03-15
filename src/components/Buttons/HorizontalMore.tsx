0061b5b27658d33b524b6b35ff7db8430ce6c2f9
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-03-15 17:38:05 +0600

    add: horizontalMore button created

---
+++ src/components/Buttons/HorizontalMore.tsx
@@ -0,0 +1,11 @@
+import { MoreHorizontal } from 'lucide-react';
+
+const HorizontalMore = () => {
+	return (
+		<button>
+			<MoreHorizontal className='dark:text-light-lighter dark:hover:text-light-primary' />
+		</button>
+	);
+};
+
+export default HorizontalMore;
