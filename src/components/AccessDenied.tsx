ba0e2ea2aaf66c89dc9747b81263485b065fbe39
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-03-15 17:37:47 +0600

    modified

--- src/components/AccessDenied.tsx
+++ src/components/AccessDenied.tsx
@@ -1,18 +1,11 @@
-import { Link } from 'react-router-dom';
+import { GoHome } from './errors/NotFound';
 
 const AccessDenied = () => {
-	console.log('AccessDenied :');
 	return (
 		<div className='flex flex-col justify-center items-center h-full'>
-			<h2 className='text-5xl text-gray-600'>403</h2>
-			<h3 className='text-gray-400'>Access Denied!</h3>
-
-			<Link
-				to='/'
-				className='hover:bg-teal-600/90 focus:ring-2 ring-teal-400/50 px-2 py-1 rounded-md text-sm text-gray-200 bg-teal-500 mt-5 transition-all duration-100'
-			>
-				Go Home
-			</Link>
+			<h2 className='text-6xl text-gray-600'>403</h2>
+			<h3 className='title text-gray-400'>Access Denied!</h3>
+			<GoHome />
 		</div>
 	);
 };
