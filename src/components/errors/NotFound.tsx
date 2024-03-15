43326b38fb272208ebe8b53c1d56d3b07f0db1d3
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-03-15 17:39:12 +0600

    fix: modify and fix unauthorized component and others

--- src/components/errors/NotFound.tsx
+++ src/components/errors/NotFound.tsx
@@ -1,7 +1,6 @@
-import { HiOutlineEmojiSad } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 
-const GoHome = () => (
+export const GoHome = () => (
 	<Link
 		to='/'
 		className='inline-block mt-8 px-3 py-2 text-sm bg-nav-selected font-DM-Sans font-semibold rounded-md'
@@ -11,38 +10,29 @@ const GoHome = () => (
 );
 
 export const Unauthorized = () => (
-	<>
-		{/* <div className='p-3' /> */}
-		<main className='flex-1 page_body pb-3'>
-			<div className='mx-3 md:mx-0 select-none overflow-hidden'>
-				<p className='mb-2 text-8xl text-cst-secondary'>
-					<HiOutlineEmojiSad />
-				</p>
-				<p className='mb-2 text-3xl font-bold text-gray-200'>Unauthorized</p>
-				<p className='text-gray-300'>Sorry! you can not access this page :(</p>
-				<GoHome />
-			</div>
-		</main>
-	</>
+	<div className='flex flex-col justify-center items-center h-full'>
+		<h2 className='text-6xl text-red/80'>401</h2>
+		<p className='flex items-center mb-3 text-3xl font-bold text-light-lighter dark:text-dark-text'>
+			Unauthorized!
+		</p>
+		<GoHome />
+	</div>
 );
 
 const NotFound = () => (
-	<>
-		{/* <div className='p-3' /> */}
-		<main className='flex justify-center items-center h-screen text-center'>
-			<div className='mx-3 md:mx-0 overflow-hidden'>
-				<p className='mb-8 text-5xl text-rose-500'>404</p>
-				<p className='flex items-center mb-3 text-3xl font-bold text-light-lighter dark:text-dark-text'>
-					<span>Page not found</span>
-					<span className='ml-1'> 😓 </span>
-				</p>
-				<p className='title text-sm text-light-lighter'>
-					Opps! the page you are looking for does&apos;t exist
-				</p>
-				<GoHome />
-			</div>
-		</main>
-	</>
+	<main className='flex justify-center items-center h-screen text-center'>
+		<div className='mx-3 md:mx-0 overflow-hidden'>
+			<p className='mb-8 text-5xl text-rose-500'>404</p>
+			<p className='flex items-center mb-3 text-3xl font-bold text-light-lighter dark:text-dark-text'>
+				<span>Page not found</span>
+				<span className='ml-1'> 😓 </span>
+			</p>
+			<p className='title text-sm text-light-lighter'>
+				Opps! the page you are looking for does&apos;t exist
+			</p>
+			<GoHome />
+		</div>
+	</main>
 );
 
 export default NotFound;
