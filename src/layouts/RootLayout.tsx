afe6fa2aefbe7ed20014605f1ab6340b79c04a4f
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-03-15 17:39:42 +0600

    fix: tried to fix layout issues and styling issues

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,21 +1,38 @@
 import { Outlet } from 'react-router-dom';
+import PermissionWrapper from '../Routes/PermissionWrapper';
 import CommunitySuggestions from '../components/CommunitySuggestions';
 import CreatePost from '../components/CreatePost';
 import Nav from '../components/Nav';
+import { UserRight } from '../contexts/user/types';
+import useAuth from '../hooks/useAuth';
 import useController from '../hooks/useController';
+import { permissions } from '../types/custom';
 
 const RootLayout = () => {
 	const {
 		state: { isPostModalOpen },
 	} = useController();
+
+	const { state } = useAuth();
+
+	const right = state?.user?.rights;
+
 	return (
 		<>
 			<Nav />
-			<div className='container mx-auto grid grid-cols-12 px-20 pt-20 h-screen overflow-hidden'>
+			<div
+				className={`container mx-auto px-20 pt-20 h-screen overflow-hidden grid ${
+					right === UserRight.FREE || !state.user
+						? 'grid-cols-1'
+						: 'grid-cols-12'
+				}`}
+			>
 				<div className='col-span-8 h-full px-28 overflow-y-auto scrollbar-none'>
 					<Outlet />
 				</div>
-				<CommunitySuggestions />
+				<PermissionWrapper links permission={permissions.all}>
+					<CommunitySuggestions />
+				</PermissionWrapper>
 			</div>
 			{isPostModalOpen ? <CreatePost isModal /> : null}
 		</>
