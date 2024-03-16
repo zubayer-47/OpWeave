8c605b9ecc8186b50f6216a041018719a0fdcaf2
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-03-17 00:01:25 +0600

    working with community page

--- src/pages/communities/Communities.tsx
+++ src/pages/communities/Communities.tsx
@@ -1,11 +1,13 @@
 import CommunityItem from './partials/CommunityItem';
+import FavCommunities from './partials/FavCommunities';
 
 const Communities = () => {
 	return (
-		<div className='py-5 h-screen'>
-			<h1 className='title text-2xl'>Communities</h1>
-			{/* <div className='mt-10 space-y-5'> */}
+		<div className='py-5 h-screen space-y-8'>
+			<FavCommunities />
+
 			<div className='py-5 space-y-5'>
+				<h1 className='title text-2xl'>Your Communities</h1>
 				<CommunityItem />
 				<CommunityItem />
 				<CommunityItem />
@@ -18,7 +20,7 @@ const Communities = () => {
 				<CommunityItem />
 				<CommunityItem />
 				<CommunityItem />
-				<CommunityItem />
+				<CommunityItem />F
 			</div>
 		</div>
 	);
