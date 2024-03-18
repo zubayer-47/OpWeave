735f5f729b96ed77cb2cc9fc35c7ce0f132d0061
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-03-19 01:11:50 +0600

    try to make it perfect

--- src/pages/communities/partials/FavCommunities.tsx
+++ src/pages/communities/partials/FavCommunities.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { signal } from '@preact/signals-react';
 import { useEffect, useRef, useState } from 'react';
 import HorizontalCommunityItem from './HorizontalCommunityItem';
 
@@ -11,114 +11,101 @@ export type Community = {
 };
 
 const FavCommunities = () => {
+	const track = signal<'prev' | 'next' | ''>('');
 	const [communities, setCommunities] = useState<Community[]>([]);
-	const [currentPage, setCurrentPage] = useState(1);
-	const scrollDiv = useRef<HTMLDivElement>(null);
+	const [currentPage, setCurrentPage] = useState(2);
+
+	const scrollContainerRef = useRef<HTMLDivElement>(null);
+	const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [_, setLoading] = useState(false);
 
 	useEffect(() => {
 		(async () => {
-			const res = await fetch(
-				`https://65218ee4a4199548356d5dc5.mockapi.io/api/v1/communities?page=${currentPage}&limit=20`
-			);
+			setLoading(true);
+			try {
+				const res = await fetch(
+					`https://65218ee4a4199548356d5dc5.mockapi.io/api/v1/communities?page=${currentPage}&limit=5`
+				);
 
-			const data = (await res.json()) as Community[];
+				const data = (await res.json()) as Community[];
 
-			setCommunities(data);
+				setCommunities(data);
+
+				console.log(track.value === 'prev', currentPage !== 0);
+
+				if (track.value === 'prev' && currentPage !== 0) {
+					const scrollWidth = scrollContainerRef.current!.scrollWidth;
+					console.log('scrollWidth :', scrollWidth);
+					setScrollPosition(scrollWidth);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+
+			setLoading(false);
 		})();
-	}, [currentPage]);
+	}, [currentPage, track.value]);
 
-	const prev = () => {
-		const elem = scrollDiv.current;
-		if (!elem) return;
+	const handleScrollLeft = () => {
+		if (scrollPosition > 0) {
+			const calculatePosition = scrollPosition - 200;
 
-		setCurrentPage((prev) => (prev !== 0 ? prev-- : 0));
+			setScrollPosition(calculatePosition);
 
-		const width = elem?.clientWidth;
-
-		elem.style.transform = `translateX(${width - width}px)`;
-
-		console.log(elem.clientWidth);
+			if (currentPage > 1) {
+				console.log('f');
+				track.value = 'prev';
+				setCurrentPage((prev) => prev - 1);
+			}
+		}
 	};
 
-	const next = () => {
-		const elem = scrollDiv.current;
-		if (!elem) return;
+	const handleScrollRight = () => {
+		const scrollWidth = scrollContainerRef.current!.scrollWidth;
+		const containerWidth = scrollContainerRef.current!.offsetWidth;
 
-		setCurrentPage((prev) => (prev !== 0 ? prev-- : 0));
+		if (scrollPosition < scrollWidth - containerWidth) {
+			const calculatePosition = scrollPosition + 200;
 
-		const width = elem?.clientWidth;
+			if (containerWidth - 100 < calculatePosition) {
+				setCurrentPage((prev) => prev + 1);
+			}
 
-		elem.style.transform = `translateX(${width + width}px)`;
-
-		console.log(elem.clientWidth);
+			setScrollPosition(calculatePosition);
+		}
 	};
+
+	console.log(track.value);
 
 	return (
-		<div className='space-y-5 relative'>
-			<h1 className='title text-2xl'>Favorite Communities</h1>
-
-			<div className='snap-x relative'>
-				<button
-					type='button'
-					onClick={prev}
-					className='absolute left-10 top-0 -translate-x-full translate-y-full z-10'
-				>
-					<ChevronLeft className='text-light-lighter size-10' />
-				</button>
-
+		<div className='relative'>
+			<button
+				className='title absolute top-0 left-2 community_suggestions rounded-full p-2 z-10'
+				onClick={handleScrollLeft}
+			>
+				&laquo;
+			</button>
+			<div className='relative overflow-x-auto snap-x snap-mandatory scrollbar-none'>
 				<div
-					ref={scrollDiv}
-					className='flex justify-start items-stretch gap-5 overflow-x-auto snap-x'
+					ref={scrollContainerRef}
+					className='flex gap-8'
+					style={{ transform: `translateX(-${scrollPosition}px)` }}
 				>
-					{!communities.length ? (
-						<p>Favorite Communities Not Found</p>
-					) : (
-						communities.map(({ avatar, bio, id, name }) => (
-							<HorizontalCommunityItem
-								avatar={avatar}
-								bio={bio}
-								key={id}
-								name={name}
-							/>
-						))
-					)}
+					{communities.map(({ id }) => (
+						<HorizontalCommunityItem key={id} name={id} />
+					))}
 				</div>
-
-				<button
-					type='button'
-					onClick={next}
-					className='absolute right-0 top-0 -translate-x-full translate-y-full'
-				>
-					<ChevronRight className='inline-block text-light-lighter size-10' />
-				</button>
 			</div>
+			<button
+				className='title absolute top-0 right-2 community_suggestions rounded-full p-2 z-10'
+				onClick={handleScrollRight}
+			>
+				&raquo;
+			</button>
 		</div>
 	);
 };
 
 export default FavCommunities;
-
-/**
- * <div className='space-y-5 relative'>
-			<h1 className='title text-2xl'>Favorite Communities</h1>
-
-			<div className='flex justify-between items-center gap-1 snap-x'>
-				<button type='button'>
-					<ChevronLeft className='text-light-lighter size-10' />
-				</button>
-
-				<div className='flex justify-start items-stretch gap-8 overflow-x-auto snap-x'>
-					<CommunityItem />
-					<CommunityItem />
-					<CommunityItem />
-					<CommunityItem />
-					<CommunityItem />
-					<CommunityItem />
-				</div>
-				<button type='button'>
-					<ChevronRight className='text-light-lighter size-10' />
-				</button>
-			</div>
-		</div>
- * 
- */
