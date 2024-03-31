47eab68ea6ef0c97667d5d81bac3db3f10661bd6
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-03-31 14:59:40 +0600

    update: button component updated and it's now reusable

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,10 +1,9 @@
+import clsx from 'clsx';
 import { RefreshCw } from 'lucide-react';
 import { ButtonHTMLAttributes } from 'react';
 
 type BtnProp = ButtonHTMLAttributes<HTMLButtonElement> & {
 	title: string;
-	handler?: () => void;
-	type?: 'button' | 'submit';
 	transparent?: boolean;
 	isDisabled?: boolean;
 	isLoading?: boolean;
@@ -12,42 +11,39 @@ type BtnProp = ButtonHTMLAttributes<HTMLButtonElement> & {
 
 const Button = ({
 	title,
-	handler,
-	type = 'button',
 	transparent,
 	isDisabled,
 	isLoading,
+	className,
 	...props
-}: BtnProp) => {
-	return (
-		<button
-			type={type}
-			// className={`px-4 p-2 rounded-lg outline-none tracking-wide ${
-			// 	(isLoading || isDisabled) && 'opacity-60'
-			// } ${
-			// 	transparent
-			// 		? 'bg-transparent text-nav-selected hover:underline'
-			// 		: 'bg-nav-selected text-light-text'
-			// }`}
-			className={`button px-4 py-2 w-fit ${
-				(isLoading || isDisabled) && 'opacity-60'
-			} ${
-				transparent
-					? 'bg-transparent text-nav-selected border border-nav-selected hover:bg-nav-selected hover:text-light-text'
-					: 'bg-nav-selected text-light-text'
-			}`}
-			onClick={handler}
-			disabled={isLoading || isDisabled}
-			{...props}
-		>
-			<span className='flex items-center'>
-				<span>{title}</span>
-				{!isLoading ? null : (
-					<RefreshCw className='ml-2 w-5 h-5 stroke-2 text-white animate-spin' />
-				)}
-			</span>
-		</button>
-	);
-};
+}: BtnProp) => (
+	<button
+		type='button'
+		// className={`px-4 p-2 rounded-lg outline-none tracking-wide ${
+		// 	(isLoading || isDisabled) && 'opacity-60'
+		// } ${
+		// 	transparent
+		// 		? 'bg-transparent text-nav-selected hover:underline'
+		// 		: 'bg-nav-selected text-light-text'
+		// }`}
+		className={clsx(
+			'button px-4 py-2 w-fit',
+			(isLoading || isDisabled) && 'opacity-60',
+			transparent
+				? 'bg-transparent text-light-primary border dark:border-dark-border hover:bg-transparent'
+				: 'bg-nav-selected text-light-text',
+			className
+		)}
+		disabled={isLoading || isDisabled}
+		{...props}
+	>
+		<span className='flex items-center'>
+			<span>{title}</span>
+			{!isLoading ? null : (
+				<RefreshCw className='ml-2 w-5 h-5 stroke-2 text-white animate-spin' />
+			)}
+		</span>
+	</button>
+);
 
 export default Button;
