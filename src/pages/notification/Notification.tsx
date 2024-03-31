24d527365f14a72571b563a62547101edbafa50d
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-03-31 12:43:08 +0600

    feat(add): Notification item's option menu added

--- src/pages/notification/Notification.tsx
+++ src/pages/notification/Notification.tsx
@@ -1,10 +1,84 @@
-import {
-	NotificationActionableItem,
-	NotificationItem,
-} from './partials/NotificationItem';
+import { useState } from 'react';
+import { NotificationItem } from './partials/NotificationItem';
+
+const data = [
+	{
+		name: 'Dr. Andrea Kerluke II',
+		createdAt: '2025-08-22T04:54:35.597Z',
+		avatar:
+			'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/223.jpg',
+		id: '7',
+		action: true,
+	},
+	{
+		name: 'Kerry Kuhn',
+		createdAt: '2097-12-26T16:45:27.890Z',
+		avatar:
+			'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/586.jpg',
+		id: '8',
+		action: false,
+	},
+	{
+		name: 'Caleb Toy',
+		createdAt: '2088-02-09T21:56:22.138Z',
+		avatar:
+			'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/537.jpg',
+		id: '9',
+		action: false,
+	},
+	{
+		name: 'Claudia Barrows',
+		createdAt: '2034-09-04T09:03:35.644Z',
+		avatar:
+			'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/948.jpg',
+		id: '10',
+		action: true,
+	},
+	{
+		name: 'Michael Lemke',
+		createdAt: '2016-03-18T06:11:49.795Z',
+		avatar:
+			'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/568.jpg',
+		id: '11',
+		action: false,
+	},
+	{
+		name: 'Brooke Bruen',
+		createdAt: '2094-01-11T05:16:06.483Z',
+		avatar:
+			'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1019.jpg',
+		id: '12',
+		action: false,
+	},
+	{
+		name: 'Shawna Medhurst',
+		createdAt: '2080-11-15T07:42:02.716Z',
+		avatar:
+			'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/886.jpg',
+		id: '13',
+		action: false,
+	},
+	{
+		name: 'Ira Stiedemann',
+		createdAt: '2052-01-23T22:18:21.875Z',
+		avatar:
+			'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/939.jpg',
+		id: '14',
+		action: true,
+	},
+];
 
 const Notification = () => {
-	// console.log({ data });
+	const [updatedOptionId, setUpdatedOptionId] = useState('');
+
+	const handleOption = (id: string) => {
+		setUpdatedOptionId(id);
+	};
+
+	const handleClose = () => {
+		setUpdatedOptionId('');
+	};
+
 	return (
 		<div className='py-5 h-screen'>
 			{/* <div className='flex justify-between items-center'>
@@ -20,23 +94,15 @@ const Notification = () => {
 			<div className='py-5 space-y-5 relative'>
 				{/* <PartialNotifications items={data} /> */}
 
-				<NotificationItem />
-				<NotificationItem />
-				<NotificationItem />
-				<NotificationItem />
-				<NotificationItem />
-				<NotificationActionableItem />
-				<NotificationActionableItem />
-				<NotificationActionableItem />
-				<NotificationItem />
-				<NotificationItem />
-				<NotificationItem />
-				<NotificationItem />
-				<NotificationItem />
-				<NotificationItem />
-				<NotificationActionableItem />
-				<NotificationItem />
-				<NotificationItem />
+				{data.map(({ id, action }) => (
+					<NotificationItem
+						action={action}
+						id={id}
+						updatedOptionId={updatedOptionId}
+						handleClose={handleClose}
+						handleOption={handleOption}
+					/>
+				))}
 			</div>
 		</div>
 	);
--- src/pages/notification/partials/NotificationItem.tsx
+++ src/pages/notification/partials/NotificationItem.tsx
@@ -1,14 +1,25 @@
-import { MoreHorizontal } from 'lucide-react';
+import { Bell, Compass } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import profile from '../../../assets/profile.webp';
 import HorizontalMore from '../../../components/Buttons/HorizontalMore';
 
-export const NotificationItem = () => {
+type Props = {
+	id: string;
+	action: boolean;
+	updatedOptionId: string;
+	handleOption: (id: string) => void;
+	handleClose: () => void;
+};
+
+export const NotificationItem = ({
+	id,
+	action,
+	updatedOptionId,
+	handleOption,
+	handleClose,
+}: Props) => {
 	return (
-		<Link
-			to={'/'}
-			className='dark:bg-dark-primary dark:hover:bg-dark-primary/50 border dark:border-dark-border transition-all p-5 flex justify-between items-center rounded-2xl'
-		>
+		<div className='dark:bg-dark-primary dark:hover:bg-dark-primary/50 border dark:border-dark-border transition-all p-5 flex justify-between items-center rounded-2xl relative'>
 			<div className='flex justify-center gap-5'>
 				<Link to='/'>
 					<img
@@ -24,43 +35,71 @@ export const NotificationItem = () => {
 					<span className='title font-normal text-sm text-dark-muted'>
 						3s ago
 					</span>
+
+					{!action ? null : (
+						<div className='flex items-center gap-3 mt-3'>
+							<button className='button text-light-text text-sm py-1.5 px-3'>
+								Accept
+							</button>
+							<button className='button-decline'>Decline</button>
+						</div>
+					)}
 				</div>
 			</div>
+			<HorizontalMore onClick={() => handleOption(id)} onBlur={handleClose} />
 
-			<HorizontalMore />
-		</Link>
-	);
-};
-
-export const NotificationActionableItem = () => {
-	return (
-		<div className='dark:bg-dark-primary dark:hover:bg-dark-primary/50 border dark:border-dark-border transition-all p-5 flex justify-between items-center rounded-2xl'>
-			<div className='flex justify-center gap-5'>
-				<Link to='/'>
-					<img
-						className='profile'
-						src={profile}
-						alt="A B M Zubayer's profile"
-					/>
-				</Link>
-				<div className='flex flex-col'>
-					<Link to='/' className='title'>
-						A B M Zubayer
-					</Link>
-					<span className='title text-sm text-dark-muted'>3s ago</span>
-
-					<div className='flex items-center gap-3 mt-3'>
-						<button className='button text-light-text text-sm py-1.5 px-3'>
-							Accept
-						</button>
-						<button className='button-decline'>Decline</button>
-					</div>
+			{updatedOptionId === id ? (
+				<div className='dark:bg-dark-primary px-1 absolute right-3 -bottom-20 flex flex-col border dark:border-dark-border rounded-xl z-10'>
+					<button
+						onClick={handleClose}
+						className='flex items-center gap-3 py-2 px-3 rounded-lg my-1.5 hover:bg-normal-primary/20 cursor-pointer transition-all'
+					>
+						<Compass className='text-light-primary' strokeWidth={1.5} />
+						<h3 className='title text-sm font-normal'>More Option</h3>
+					</button>
+					<hr className='border-t-2 dark:border-dark-border' />
+					<button
+						onClick={handleClose}
+						className='flex items-center gap-3 py-2 px-3 rounded-lg my-1.5 hover:bg-normal-primary/20 cursor-pointer transition-all'
+					>
+						<Bell className='text-light-primary' strokeWidth={1.5} />
+						<h3 className='title text-sm font-normal'>More Option</h3>
+					</button>
 				</div>
-			</div>
-
-			<button>
-				<MoreHorizontal className='dark:text-light-lighter dark:hover:text-light-primary' />
-			</button>
+			) : null}
 		</div>
 	);
 };
+
+// export const NotificationActionableItem = ({ id }: Props) => {
+// 	return (
+// 		<div className='dark:bg-dark-primary dark:hover:bg-dark-primary/50 border dark:border-dark-border transition-all p-5 flex justify-between items-center rounded-2xl'>
+// 			<div className='flex justify-center gap-5'>
+// 				<Link to='/'>
+// 					<img
+// 						className='profile'
+// 						src={profile}
+// 						alt="A B M Zubayer's profile"
+// 					/>
+// 				</Link>
+// 				<div className='flex flex-col'>
+// 					<Link to='/' className='title'>
+// 						A B M Zubayer
+// 					</Link>
+// 					<span className='title font-normal text-sm text-dark-muted'>
+// 						3s ago
+// 					</span>
+
+// 					<div className='flex items-center gap-3 mt-3'>
+// 						<button className='button text-light-text text-sm py-1.5 px-3'>
+// 							Accept
+// 						</button>
+// 						<button className='button-decline'>Decline</button>
+// 					</div>
+// 				</div>
+// 			</div>
+
+// 			<HorizontalMore />
+// 		</div>
+// 	);
+// };
