3d0492e30aba310920d771fbb5f47ebc5e0c9424
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-04-02 17:30:09 +0600

    feat(add): user profile's time added

--- src/pages/userProfile/partials/Timeline.tsx
+++ src/pages/userProfile/partials/Timeline.tsx
@@ -1,5 +1,16 @@
+import Post from '../../../components/Post';
+import CenterLayout from '../../../layouts/CenterLayout';
+
 const Timeline = () => {
-	return <div>Timeline</div>;
+	return (
+		<CenterLayout className='max-w-102 w-full my-10'>
+			<div className='space-y-10'>
+				{new Array(10).fill(true).map(() => (
+					<Post />
+				))}
+			</div>
+		</CenterLayout>
+	);
 };
 
 export default Timeline;
