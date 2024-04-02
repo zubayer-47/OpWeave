5d1da5e2a155dcc2f2749a5834e8d7fa12b6c330
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-04-02 17:31:00 +0600

    feat(add): user profile's communities tab added

--- src/pages/userProfile/partials/Communities.tsx
+++ src/pages/userProfile/partials/Communities.tsx
@@ -1,5 +1,41 @@
+import { useState } from 'react';
+import data from '../../../../data.json';
+import CenterLayout from '../../../layouts/CenterLayout';
+import CommunityItem from '../../communities/partials/CommunityItem';
+
+const slicedData = data.slice(10, 20);
+
 const Communities = () => {
-	return <div>Communities</div>;
+	const [updatedOptionId, setUpdatedOptionId] = useState('');
+
+	const handleOption = (id: string) => {
+		setUpdatedOptionId(id);
+	};
+
+	const handleClose = () => {
+		setUpdatedOptionId('');
+	};
+
+	return (
+		<CenterLayout className='max-w-102 w-full my-10'>
+			<div className='py-5 space-y-5'>
+				<h1 className='title text-2xl'>Communities you're in</h1>
+				{slicedData.map(({ id, createdAt, name }) => (
+					<CommunityItem
+						key={id}
+						id={id}
+						// avatar={avatar}
+						bio={name}
+						name={name}
+						createdAt={createdAt}
+						updatedOptionId={updatedOptionId}
+						handleClose={handleClose}
+						handleOption={handleOption}
+					/>
+				))}
+			</div>
+		</CenterLayout>
+	);
 };
 
 export default Communities;
