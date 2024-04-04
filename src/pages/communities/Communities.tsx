a665718bfeba3553fb87645f654c1a6b032a6d65
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-04-04 16:53:51 +0600

    working with community page

--- src/pages/communities/Communities.tsx
+++ src/pages/communities/Communities.tsx
@@ -3,12 +3,22 @@
 import { useState } from 'react';
 import CommunityItem from './partials/CommunityItem';
 
+import { Plus } from 'lucide-react';
 import data from '../../../data.json';
+import Form from '../../components/Forms/Form';
+import ModalLayout from '../../layouts/ModalLayouts/ModalLayout';
 
 const slicedData = data.slice(10, 20);
 
 const Communities = () => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [updatedOptionId, setUpdatedOptionId] = useState('');
+
+	const handleFormSubmit = (data: unknown) => {
+		console.log('Form data:', data);
+		// Do something with the form data
+		setIsModalOpen(false); // Close the modal after submission
+	};
 
 	const handleOption = (id: string) => {
 		setUpdatedOptionId(id);
@@ -23,7 +33,16 @@ const Communities = () => {
 			{/* // TODO: customize it */}
 			{/* <FavCommunities /> */}
 			<div className='py-5 space-y-5'>
-				<h1 className='title text-2xl'>Your Communities</h1>
+				<div className='flex justify-between items-center'>
+					<h1 className='title text-2xl'>Your Communities</h1>
+					<button
+						type='button'
+						className='community_suggestions rounded-full p-1'
+						onClick={() => setIsModalOpen(true)}
+					>
+						<Plus className='text-light-lighter hover:text-light-primary transition-colors' />
+					</button>
+				</div>
 				{slicedData.map(({ id, createdAt, name }) => (
 					<CommunityItem
 						key={id}
@@ -38,6 +57,10 @@ const Communities = () => {
 					/>
 				))}
 			</div>
+
+			<ModalLayout isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+				<Form onSubmit={handleFormSubmit} />
+			</ModalLayout>
 		</div>
 	);
 };
