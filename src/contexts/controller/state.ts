323188f1e5c482d1bbaf82bbcd3bb59734e0e323
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-04-26 23:51:15 +0600

    replace these by redux

--- src/contexts/controller/state.ts
+++
@@ -1,6 +0,0 @@
-import { ControllerStateTypes } from './types';
-
-export const initControllerState: ControllerStateTypes = {
-	notification: null,
-	isPostModalOpen: false,
-};
--- src/contexts/modal/state.ts
+++
@@ -1,5 +0,0 @@
-import { ModalStateType } from './types';
-
-export const initModalState: ModalStateType = {
-	isVisibleAuthModal: false,
-};
