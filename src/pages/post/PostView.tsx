7ca7b4f3f4ba3b633f9074f5d4bc22dc3d370727
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-05-15 23:33:25 +0600

    add single post view page

---
+++ src/pages/post/PostView.tsx
@@ -0,0 +1,23 @@
+import { skipToken } from '@reduxjs/toolkit/query';
+import { useLocation } from 'react-router-dom';
+import Post from '../../components/Post';
+import { useGetPostQuery } from '../../features/post/postApi';
+
+const PostView = () => {
+	const { state } = useLocation();
+	const { data, isLoading, isSuccess } = useGetPostQuery(
+		{ community_id: state.community_id, post_id: state.post_id } || skipToken
+	);
+
+	if (isLoading) return <h1 className='title'>Loading</h1>;
+
+	return (
+		isSuccess && (
+			<div className='mb-5'>
+				<Post post={data} />
+			</div>
+		)
+	);
+};
+
+export default PostView;
